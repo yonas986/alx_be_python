d8f459c979258c22dddc9e9da04dd984fc9016c7
yonas986 <yonasbegashaw10@gmail.com>
2025-09-13 22:52:01 +0300

    Update basic_operations.py

--- alx_be_python/python_introduction/basic_operations.py
+++ alx_be_python/python_introduction/basic_operations.py
@@ -8,3 +8,4 @@ product_result = number1 * number2
 print(f"Sum of {number1} and {number2} is {sum_result}")
 print(f"Difference of {number1} and {number2} is {difference_result}")
 print(f"Product of {number1} and {number2} is {product_result}")
+
